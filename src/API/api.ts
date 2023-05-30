99ad321ea8d994f75acb4982b16653d10b47e068
showermax <sevenhauz@gmail.com>
2023-05-30 23:58:04 +0200

    types

--- src/API/api.ts
+++ src/API/api.ts
@@ -1,21 +1,34 @@
 import axios from "axios";
 
-const id_List='52a31596-abb7-49a5-a9c4-3fb4e24acdef'
+type ListType =  {
+        id: string,
+        title: string,
+        addedDate: string,
+        order: number
+    }
+type GenericType<T> = {
+    resultCode: number
+    messages: Array<string>,
+    data: T
+}
+
+
+const id_List='94ca0ed3-3654-4f85-ae98-23e46a915c42'
 export const api = {
     getLists () {
-        return axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists',
+        return axios.get<ListType[]>('https://social-network.samuraijs.com/api/1.1/todo-lists',
             {"withCredentials": true})
     },
     addList () {
-        return axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists',
+        return axios.post<GenericType<{ item: ListType }>>('https://social-network.samuraijs.com/api/1.1/todo-lists',
             {title:"Inbox"},{"withCredentials": true})
     },
     deleteList () {
-        return axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists${id_List}`,
+        return axios.delete<GenericType<{}>>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${id_List}`,
             {"withCredentials": true})
     },
     updateList () {
-        return axios.put(`https://social-network.samuraijs.com/api/1.1/todo-lists${id_List}`,
+        return axios.put(`https://social-network.samuraijs.com/api/1.1/todo-lists/${id_List}`,
             {title:"Inbox2"},{"withCredentials": true})
     }
 }
